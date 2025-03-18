58525ab61bbd29fdb69f11193b9a45f95b06b6aa
Thom Mott <thommott@proton.me>
2025-03-18 01:39:29 -0700

    first edit in forever

--- src/scenes/Menu.ts
+++ src/scenes/Menu.ts
@@ -2,11 +2,10 @@ import { GameConfig, UIConfig } from "../config";
 import { KeyMap } from "../keymap";
 import { gVar, gConst, saveCookie } from "../global";
 import { SoundMan } from "../soundman";
-import { Math as pMath } from 'phaser';
+import { GameObjects, Math as pMath } from 'phaser';
 
 var hWidth;
 var hHeight;
-var typeTime = 0.5;
 
 export class MenuScene extends Phaser.Scene {
     private hsText  : Phaser.GameObjects.Text;
@@ -14,12 +13,12 @@ export class MenuScene extends Phaser.Scene {
     private menu    : Phaser.GameObjects.Container;
     private overlay : Phaser.GameObjects.Container;
     private creditsOverlay : Phaser.GameObjects.Container;
-    
-    private typeDelay : number;
-
-    // typing shiiii
-    private passage : string;
-    private charWritten : number;
+   
+    private textTimer : Phaser.Time.TimerEvent;
+    private textTimerComplete : Phaser.Time.TimerEvent;
+    private typeDelay : number  = 2; // in ms
+    private isTyping  : boolean = false;
+    private playTypeSound : boolean = true;
 
     constructor() {
         super({ key: 'MenuScene' });
@@ -33,7 +32,7 @@ export class MenuScene extends Phaser.Scene {
         hWidth  = UIConfig.hWidth;
 
         this.text = this.add.text(UIConfig.borderPadding, UIConfig.borderPadding, '', gConst.uiConfig);
-        var menu = this.add.container(UIConfig.hWidth, UIConfig.hHeight - (UIConfig.hHeight / 16));
+        var menu  = this.add.container(UIConfig.hWidth, UIConfig.hHeight - (UIConfig.hHeight / 16));
 
         // menu container
         let playText = this.add.text(0, 0, "PLAY", gConst.settingsConfig).setOrigin(0.5);
@@ -109,21 +108,31 @@ export class MenuScene extends Phaser.Scene {
         });
 
         KeyMap.keyEXIT.onDown = () => {
+            if (this.isTyping) {
+                this.textTimer.paused = true;
+                this.playTypeSound = false;
+                let remaining = this.textTimer.getRepeatCount();
+                
+                for (let i = 0; i < remaining; i++) {
+                    this.textTimer.callback();
+                }
+
+                this.playTypeSound = true;
+                this.textTimerComplete.remove(true);
+            }
+
             if (this.creditsOverlay.visible) {
                 this.creditsOverlay.setVisible(false);
             }
         }
 
         menu.add([playText, tutorialText, creditText]);
-
         this.menu = menu;
         this.menu.setVisible(false);
-        this.typeText(gConst.menuText);
 
 
         // Credits Overlay
         this.creditsOverlay = this.add.container(hWidth, hHeight);
-
         var rect = this.add.rectangle(0, 0, hWidth * 2, hHeight * 2, 0, 1).setOrigin(0.5);
 
         // If I have time, make the game type this.
@@ -132,67 +141,14 @@ export class MenuScene extends Phaser.Scene {
 
         this.creditsOverlay.add([rect, credits, creditsPrompt]);
         this.creditsOverlay.setVisible(false);
+
+        // Start Typing
+        this.printText(gConst.menuText, this.text, this.TriggerMenu);
     }
 
 
     update(time : number, delta : number) : void {
         delta /= 1000;
-
-
-        // Render Text
-        this.typeDelay -= delta;
-        if (this.charWritten >= this.passage.length || 0 < this.typeDelay) return;
-
-        let start = this.charWritten;
-        let end = start;
-        let chunk = "";
-
-        // Step 1: Find chunk up to 8 characters or stopping at space/newline
-        while (end < this.passage.length && end - start < 8) {
-            let currentChar = this.passage[end];
-
-            if (currentChar == ' ' && start == this.charWritten) {
-                // If we start on spaces, extend until we hit a non-space
-                while (end < this.passage.length && this.passage[end] === ' ') {
-                    chunk += this.passage[end];
-                    end++;
-                }
-                break;
-            }
-
-            chunk += currentChar;
-            end++;
-        }
-
-        // Step 2: Append the chunk and advance pointer
-        this.text.text += chunk;
-        this.charWritten = end;
-
-        // Step 3: Determine typeDelay
-        if (chunk.includes('\n')) {
-            this.typeDelay = typeTime;
-        } else if (chunk.includes(' ')) {
-            this.typeDelay = delta;
-        }
-
-        // Step 4: Handle sound playing condition
-        // TODO: make newline ding.
-        if (chunk.includes('\n')) {
-            SoundMan.play('typing');
-        } // TODO: Downshift and reverb typing sound for now.
-        else if (!chunk.includes('\r') && !chunk.includes('\0')) {
-            SoundMan.play('typing');
-        }
-
-        if (this.charWritten == this.passage.length) {
-            this.TriggerMenu();
-        }
-    }
-
-    typeText(passage : string) : void {
-        this.passage = passage;
-        this.charWritten = 0;
-        this.typeDelay = 0;
     }
 
     changeScene(key : string) : void {
@@ -213,5 +169,36 @@ export class MenuScene extends Phaser.Scene {
 
     toggleOverlay() : void {
         return;
+    }
+
+
+    printText(str : string, dialog : GameObjects.Text, onEnd : () => void) : void {
+        this.isTyping = true;
+        dialog.text = '';
+        let currentChar = 0;
+        let offset = Math.round(str.length * 0.01);
+        this.textTimer = this.time.addEvent({
+            delay: this.typeDelay,
+            repeat: dialog.text.length - 1,
+            callback: () => { 
+                dialog.text += str.substring(currentChar, currentChar + offset);
+                currentChar += offset++;
+                if (this.playTypeSound) {
+                    SoundMan.play('typing');
+                }
+            },
+            callbackScope: this
+        });
+
+        // OnComplete
+        this.time.addEvent({
+            delay: (this.typeDelay * dialog.text.length),
+            callback: () => {
+                this.textTimer.destroy();
+                onEnd();
+                this.isTyping = false;
+            },
+            callbackScope: this
+        });
     }
 }
